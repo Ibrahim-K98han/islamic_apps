ed62bfcb33912ecd6383655f475a2982edfe96c8
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-03-13 09:04:50 +0600

    gosole module done

--- lib/pages/gosole_page.dart
+++ lib/pages/gosole_page.dart
@@ -35,70 +35,52 @@ class _GosolePageState extends State<GosolePage> {
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
                             color: Colors.grey)),
-                    SizedBox(height: 10,),
-                    Text('''প্রয়োজন অপ্রয়োজনে আমরা প্রতিদিন গোসল করি। তবে পবিত্রতা অর্জনের উদ্দেশ্যে গোসলের ক্ষেত্রে কিছু নিয়ম মানতে হয়। তা না হলে পবিত্রতা হওয়া যায় না।চলুন তাহলে গোসলের নিয়ম সম্পর্কে বিস্তারিত জেনে নেই –'''),
-                    Text('''আল্লাহুম্মাফ তাহলি আবওয়াবা রহমাতিক'''),
-                    Text('''হে আল্লাহ! আমার জন্য আপনার রহমতের দরজাগুলো খুলে দিন'''),
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: Text('''মসজিদ থেকে বের হওয়ার দোয়া''',
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey)),
+                    SizedBox(
+                      height: 10,
                     ),
-                    SizedBox(height: 10,),
-                    Text('''اَللّٰهمَّ اِنِّي اَسْٮَٔلُكَ مِنْ فَضْلِكِ'''),
-                    Text('''আল্লাহুম্মা ইন্নী আসআলূকা মিস ফাযলিকা'''),
-                    Text('''হে আল্লাহ আমি আপনার দয়া প্রার্থনা করি'''),
+                    Text(
+                        '''প্রয়োজন অপ্রয়োজনে আমরা প্রতিদিন গোসল করি। তবে পবিত্রতা অর্জনের উদ্দেশ্যে গোসলের ক্ষেত্রে কিছু নিয়ম মানতে হয়। তা না হলে পবিত্রতা হওয়া যায় না।চলুন তাহলে গোসলের নিয়ম সম্পর্কে বিস্তারিত জেনে নেই –'''),
+                    Text('''গোসলের দোয়া''',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                        '''বাংলা উচ্চারণঃ “নাওয়াইতুয়ান গোছলা লিরাফিল জানাবা-তি।'''),
+                    Text('''ফরজ গোসলের নিয়ত''',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('''ববাংলা উচ্চারণঃ “নাওয়াইতুল গুছলা লিরাফইল জানাবাতি।
+                      অর্থঃ আমি নাপাকি থেকে পাক হওয়ার জন্য গোসল করছি।'''),
+                    Text('''ফরজ গোসলের নিয়ম''',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey)),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('''নিম্নে ফরজ গোসলের নিয়ম দেওয়া হলো –
+
+গোসলের দোয়া পড়া
+তারপর গোসলের নিয়ত করে বিসমিল্লাহ বলে দু হাত কব্জি পর্যন্ত ভালো করে ধৌত করতে হবে।
+এরপর শরীরের কোন জায়গায় অপবিত্র বস্তু লেগে থাকলে তা পরিষ্কার করতে হবে।
+তারপর অজু করতে হবে। গড়গড়ার সাথে কুলি করতে হবে, রোজাদার হলে গড়গড়া করা যাবে না। তিনবার কুলি করা সুন্নত
+তারপর তিনবার নাকে পানি দিয়ে নাক পরিষ্কার করতে হবে
+অজুর পর মাথায় এমনভাবে পানি ঢালতে হবে যেন চুলের গোড়া পর্যন্ত পানি পৌঁছে যায়।
+এরপর ডান কাঁধে পরে বাম কাঁধে পানি ঢেলে সমস্ত শরীর ধৌত করতে হবে যেন শরীরের কোন অংশ শুকনো না থাকে।
+সর্বশেষে পা ধুতে হবে
+এরপর সারা শরীর কোন কাপড় বা গামছা দিয়ে মুছে শুকনো কাপড় পড়তে হবে।'''),
                   ],
                 ),
-                SizedBox(height: 20,),
-                Center(
-                  child: Text('''মসজিদে প্রবেশ করার সুন্নত ৫টি''',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey)),
-                ),
-                SizedBox(height: 10,),
-
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('১. বিসমিল্লাহ বলা'),
-                    Text('২. দরূদ শরীফ পড়া'),
-                    Text('৩. দো’আ পড়া'),
-                    Text('৪. মসজিদে প্রবেশ করার সময় প্রথমে ডান পা ঢুকান'),
-                    Text('৫.  এ’তেকাফের নিয়ত করা'),
-                  ],
-                ),
-
-                SizedBox(height: 20,),
-                Center(
-                  child: Text('''মসজিদে বের হওয়ার সুন্নত ৫টি''',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.grey)),
-                ),
-                SizedBox(height: 10,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('১. বিসমিল্লাহ বলা'),
-                    Text('২. দরূদ শরীফ পড়া'),
-                    Text('৩. দো’আ পড়া'),
-                    Text('৪. প্রথমে বাম পা মসজিদ হতে বের করা'),
-                    Text(' ৫. ডান পায়ে জুতা আগে পরিধান করা'),
-                  ],
-                )
-
               ],
             ),
           ),
