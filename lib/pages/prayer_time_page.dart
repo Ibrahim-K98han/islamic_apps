03c1e37ed690ce176d39abaa9e6746eba1e05a8f
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-03-25 23:59:14 +0600

    prayer time api added

--- lib/pages/prayer_time_page.dart
+++ lib/pages/prayer_time_page.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../json_connection.dart';
-import '../model/data.dart';
 
 class PrayTimes extends StatefulWidget {
   PrayTimes({super.key});
@@ -14,27 +12,43 @@ class PrayTimes extends StatefulWidget {
 
 class _PrayTimesState extends State<PrayTimes> {
 
-  JsonConnection jsonConnection = new JsonConnection();
-  late Data list;
+  // JsonConnection jsonConnection = new JsonConnection();
+  // late Data list;
 
   static String city = 'Dhaka';
   static String country = 'Bangladesh';
-  static int method = 1;
+  static int method = 8;
+  var data;
 
   final String url =
-      'http://api.aladhan.com/v1/calendar/2023/4?latitude=23.810331&longitude=90.412521&method=$method';
-
-  Future getPTData() async {
-    http.Response res = await http.get(Uri.parse(url), headers: {
-      "Accept":
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+      'http://api.aladhan.com/v1/timingsByCity?city=$city&country=$country&method=$method';
+  Future getData()async{
+    var response = await http.get(Uri.parse(url));
+    setState(() {
+      var decode = json.decode(response.body);
+      data = decode["data"];
+      print(data);
     });
-    final data = jsonDecode(res.body);
-
-    list = Data.fromJson(data);
-
-    return list;
   }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getData();
+  }
+
+
+  // Future getPTData() async {
+  //   http.Response res = await http.get(Uri.parse(url), headers: {
+  //     "Accept":
+  //     "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+  //   });
+  //   final data = jsonDecode(res.body);
+  //
+  //   list = Data.fromJson(data);
+  //
+  //   return list;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -43,48 +57,14 @@ class _PrayTimesState extends State<PrayTimes> {
         title: Text('Pray Times'),
         backgroundColor: Colors.blue,
       ),
-      body: FutureBuilder(
-        //future: jsonConnection.getPTLocation(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text('Fajr'),
-                          Text('Dhuhr'),
-                          Text('Asr'),
-                          Text('Maghrib'),
-                          Text('Isha'),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text(snapshot.data.data.timings.fajr),
-                          Text(snapshot.data.data.timings.dhuhr),
-                          Text(snapshot.data.data.timings.asr),
-                          Text(snapshot.data.data.timings.maghrib),
-                          Text(snapshot.data.data.timings.isha),
-                        ],
-                      ),
-                    ),
-                    Padding(padding: const EdgeInsets.all(8.0),
-                      child: Column(children: <Widget>[
-                        Text(snapshot.data.data.meta.timezone),
-                      ],),)
-                  ],
-                ),
-              ),
-            );
-          }
-      ),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(),
+          );
+        },
+      )
     );
   }
 }
